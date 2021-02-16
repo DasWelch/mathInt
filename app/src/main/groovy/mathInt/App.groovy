1bc6aa56f8d4a0055a9b9e327683d657f8c39250
DasWelch <35710104+DasWelch@users.noreply.github.com>
2021-02-16 13:54:48 -0500

    going to need to use a different data structure

--- app/src/main/groovy/mathInt/App.groovy
+++ app/src/main/groovy/mathInt/App.groovy
@@ -10,19 +10,33 @@ class App {
             num.setLength(0)
         }
     }
+    static void findOpPosition(StringBuilder num, ArrayList<objectStore> istack){
+        //PE MDAS
+        Integer number = num.toInteger();
+        for(int i=istack.size(); i>=0; i--) {
+            if(istack[i]){ // if our operation equals the thing were looing at then add it to the array next
+
+            }
+
+        }
+    }
 
     static void main(String[] args) {
-        String Solve = "55 + 4 *( 2 - 3 ) / 4"
+        String Solve = "2 + 4 * 20"//"55 + 4 *( 2 - 3 ) / 4" //"4*3+2"
         StringBuilder num = new StringBuilder()
+//        int parenths = 0
+        int[] nextopindex = 0;
 
         ArrayList<objectStore> istack = new ArrayList<objectStore>(10)
-
+        //creating of stack
         for (int i = 0; i < Solve.length(); i++) {
             if (Solve[i].equals("+")) {
-
                 addNumToIstack(num, istack)
+                findOpPosition();
+                //find correct position in list to add operation
+                //add that operation then
+                // we move on to see what is next (it should be a number)
                 istack.add(new objectStore(null, "+"))
-
             } else if (Solve[i].equals("-")) {
                 addNumToIstack(num, istack)
                 istack.add(new objectStore(null, "-"))
@@ -31,21 +45,27 @@ class App {
                 istack.add(new objectStore(null, "*"))
             } else if (Solve[i].equals("/")) {
                 addNumToIstack(num, istack)
+
                 istack.add(new objectStore(null, "/"))
             }
 
-            else if (Solve[i].equals("(")) {
-                addNumToIstack(num, istack)
-                istack.add(new objectStore(null, "("))
-            }
-            else if (Solve[i].equals(")")) {
-                addNumToIstack(num, istack)
-                istack.add(new objectStore(null, ")"))
-            }
+//            else if (Solve[i].equals("(")) {
+//                addNumToIstack(num, istack)
+////                parenths++; // count the parents to see how deep we are. higher the num deeper we are
+//                // store start index in the array
+//                istack.add(new objectStore(null, "("))
+//            }
+//            else if (Solve[i].equals(")")) {
+//                addNumToIstack(num, istack)
+//                istack.add(new objectStore(null, ")"))
+//            }
 
             else if (!Solve[i].equals(" ")) {
                 num.append(Solve[i])
 
+            }
+            if(i == Solve.length()-1){
+                addNumToIstack(num, istack)
             }
 
             for (ob in istack) {
@@ -53,6 +73,12 @@ class App {
             }
             println()
         }
+        // stack sorting
+        ArrayList<objectStore> sortedIstack = new ArrayList<objectStore>(10)
+
+
+
+
     }
 }
 
@@ -73,4 +99,8 @@ class objectStore {
         this.type = type
     }
 
+}
+
+enum ops{
+    PLUS(1), MINUS(2), MULT(3), DIV(4)
 }
